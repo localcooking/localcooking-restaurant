1e1c67dedfdcb854931c5572517a79c6ac73543a
Athan Clark <athan.clark@localcooking.com>
2018-03-31 14:51:46 -0600

    registration page content

--- frontend/src/React/ReCaptcha.purs
+++ frontend/src/React/ReCaptcha.purs
@@ -1,17 +1,37 @@
-module React.ReCaptcha where
+module React.ReCaptcha
+  ( reCaptcha, ReCaptchaResponse
+  ) where
 
-import Prelude (Unit)
-import React (ReactClass, Event, ReactElement, createElement)
+import Prelude
+import React (ReactClass, ReactElement, createElement)
+import Data.Argonaut (class EncodeJson, class DecodeJson, encodeJson, decodeJson)
+import Data.Generic (class Generic, gEq, gShow)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Uncurried (EffFn1)
 
 
 foreign import reCaptchaImpl :: forall props. ReactClass props
 
+newtype ReCaptchaResponse = ReCaptchaResponse Int
+
+derive instance genericReCaptchaResponse :: Generic ReCaptchaResponse
+
+instance eqReCaptchaResponse :: Eq ReCaptchaResponse where
+  eq = gEq
+
+instance showReCaptchaResponse :: Show ReCaptchaResponse where
+  show = gShow
+
+instance encodeJsonReCaptchaResponse :: EncodeJson ReCaptchaResponse where
+  encodeJson (ReCaptchaResponse x) = encodeJson x
+
+instance decodeJsonReCaptchaResponse :: DecodeJson ReCaptchaResponse where
+  decodeJson json = ReCaptchaResponse <$> decodeJson json
+
 
 type ReCaptchaProps eff =
   { sitekey :: String
-  , verifyCallback :: EffFn1 eff Event Unit
+  , verifyCallback :: EffFn1 eff ReCaptchaResponse Unit
   , onloadCallback :: Eff eff Unit
   }
 
--- frontend/src/Spec/Content/Register.purs
+++ frontend/src/Spec/Content/Register.purs
@@ -3,6 +3,8 @@ module Spec.Content.Register where
 import Types.Env (env)
 
 import Prelude
+import Data.Maybe (Maybe (..))
+import Text.Email.Validate (emailAddress)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Uncurried (mkEffFn1)
@@ -10,22 +12,52 @@ import Control.Monad.Eff.Uncurried (mkEffFn1)
 import Thermite as T
 import React as R
 import React.DOM as R
-import React.ReCaptcha (reCaptcha)
+import React.ReCaptcha (reCaptcha, ReCaptchaResponse)
 
 import MaterialUI.Typography (typography)
 import MaterialUI.Typography as Typography
+import MaterialUI.Divider (divider)
+import MaterialUI.TextField (textField)
+import MaterialUI.Input as Input
 
 import Unsafe.Coerce (unsafeCoerce)
 
 
-type State = Unit
+type State =
+  { reCaptcha :: Maybe ReCaptchaResponse
+  , email :: String
+  , emailDirty :: Maybe Boolean
+  , emailConfirm :: String
+  , emailConfirmDirty :: Maybe Boolean
+  , password :: String
+  , passwordDirty :: Maybe Boolean
+  , passwordConfirm :: String
+  , passwordConfirmDirty :: Maybe Boolean
+  }
 
 initialState :: State
-initialState = unit
+initialState =
+  { reCaptcha: Nothing
+  , email: ""
+  , emailDirty: Nothing
+  , emailConfirm: ""
+  , emailConfirmDirty: Nothing
+  , password: ""
+  , passwordDirty: Nothing
+  , passwordConfirm: ""
+  , passwordConfirmDirty: Nothing
+  }
 
 data Action
-  = GotReCaptchaVerify R.Event
-  | ReCaptchaLoaded
+  = GotReCaptchaVerify ReCaptchaResponse
+  | ChangedEmail String
+  | ChangedEmailConfirm String
+  | EmailUnfocused
+  | EmailConfirmUnfocused
+  | ChangedPassword String
+  | ChangedPasswordConfirm String
+  | PasswordUnfocused
+  | PasswordConfirmUnfocused
 
 
 type Effects eff =
@@ -37,22 +69,93 @@ spec :: forall eff. T.Spec (Effects eff) State Unit Action
 spec = T.simpleSpec performAction render
   where
     performAction action props state = case action of
-      GotReCaptchaVerify e -> do
-        liftEff $ log $ unsafeCoerce e
-      ReCaptchaLoaded -> do
-        liftEff $ log "Loaded ReCaptcha"
+      GotReCaptchaVerify e -> void $ T.cotransform _ { reCaptcha = Just e }
+      ChangedEmail e -> void $ T.cotransform _ { email = e, emailDirty = Just false }
+      ChangedEmailConfirm e -> void $ T.cotransform _ { emailConfirm = e, emailConfirmDirty = Just false }
+      ChangedPassword e -> void $ T.cotransform _ { password = e, passwordDirty = Just false }
+      ChangedPasswordConfirm e -> void $ T.cotransform _ { passwordConfirm = e, passwordConfirmDirty = Just false }
+      EmailUnfocused -> void $ T.cotransform _ { emailDirty = Just true }
+      EmailConfirmUnfocused -> void $ T.cotransform _ { emailConfirmDirty = Just true }
+      PasswordUnfocused -> void $ T.cotransform _ { passwordDirty = Just true }
+      PasswordConfirmUnfocused -> void $ T.cotransform _ { passwordConfirmDirty = Just true }
+
 
     render :: T.Render State Unit Action
     render dispatch props state children =
       [ typography
-        { variant: Typography.headline
+        { variant: Typography.display2
         , align: Typography.center
         , color: Typography.primary
         } [R.text "Register"]
+      , divider {}
+      , textField
+        { label: R.text "Email"
+        , fullWidth: true
+        , onChange: mkEffFn1 \e -> dispatch $ ChangedEmail (unsafeCoerce e).target.value
+        , onBlur: mkEffFn1 \_ -> dispatch EmailUnfocused
+        , error: case emailAddress state.email of
+            Nothing -> state.emailDirty == Just true
+            Just _ -> case state.emailConfirmDirty of
+              Nothing -> false
+              Just dirty
+                | dirty -> state.email == state.emailConfirm
+                | otherwise -> false
+        , name: "register-email"
+        , id: "register-email"
+        }
+      , textField
+        { label: R.text "Email Confirm"
+        , fullWidth: true
+        , onChange: mkEffFn1 \e -> dispatch $ ChangedEmailConfirm (unsafeCoerce e).target.value
+        , onBlur: mkEffFn1 \_ -> dispatch EmailConfirmUnfocused
+        , error: case emailAddress state.emailConfirm of
+            Nothing -> state.emailConfirmDirty == Just true
+            Just _ -> case state.emailDirty of
+              Nothing -> false
+              Just _ -> state.email == state.emailConfirm
+        , name: "register-email-confirm"
+        , id: "register-email-confirm"
+        }
+      , textField
+        { label: R.text "Password"
+        , fullWidth: true
+        , "type": Input.passwordType
+        , onChange: mkEffFn1 \p -> dispatch $ ChangedPassword (unsafeCoerce p).target.value
+        , onBlur: mkEffFn1 \_ -> dispatch PasswordUnfocused
+        , error: case state.passwordDirty of
+            Nothing -> false
+            Just dirty
+              | not dirty -> false
+              | otherwise -> case state.passwordConfirmDirty of
+                Nothing -> false
+                Just dirty
+                  | not dirty -> false
+                  | otherwise -> state.password == state.passwordConfirm
+        , name: "register-password"
+        , id: "register-password"
+        }
+      , textField
+        { label: R.text "Password Confirm"
+        , fullWidth: true
+        , "type": Input.passwordType
+        , onChange: mkEffFn1 \p -> dispatch $ ChangedPasswordConfirm (unsafeCoerce p).target.value
+        , onBlur: mkEffFn1 \_ -> dispatch PasswordConfirmUnfocused
+        , error: case state.passwordConfirmDirty of
+            Nothing -> false
+            Just dirty
+              | not dirty -> false
+              | otherwise -> case state.passwordDirty of
+                Nothing -> false
+                Just dirty
+                  | not dirty -> false
+                  | otherwise -> state.password == state.passwordConfirm
+        , name: "register-password-confirm"
+        , id: "register-password-confirm"
+        }
       , reCaptcha
         { sitekey: env.googleReCaptchaSiteKey
         , verifyCallback: mkEffFn1 (dispatch <<< GotReCaptchaVerify)
-        , onloadCallback: dispatch ReCaptchaLoaded
+        , onloadCallback: pure unit
         }
       ]
 
