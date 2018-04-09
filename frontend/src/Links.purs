86495db3f6f4aa204912fb35d7493aca57a6d70d
Athan Clark <athan.clark@localcooking.com>
2018-04-09 17:42:10 -0600

    using nginx for static images

--- frontend/src/Links.purs
+++ frontend/src/Links.purs
@@ -65,10 +65,10 @@ data AboutPageLinks
 
 instance toLocationAboutPageLinks :: ToLocation AboutPageLinks where
   toLocation x = case x of
-    Paragraph1Png -> Location (Right $ rootDir </> dir "images" </> file "paragraph1-image.png") Nothing Nothing
-    Paragraph2Png -> Location (Right $ rootDir </> dir "images" </> file "paragraph2-image.png") Nothing Nothing
-    Paragraph3Png -> Location (Right $ rootDir </> dir "images" </> file "paragraph3-image.png") Nothing Nothing
-    Paragraph4Png -> Location (Right $ rootDir </> dir "images" </> file "paragraph4-image.png") Nothing Nothing
+    Paragraph1Png -> Location (Right $ rootDir </> dir "static" </> dir "images" </> file "paragraph1-image.png") Nothing Nothing
+    Paragraph2Png -> Location (Right $ rootDir </> dir "static" </> dir "images" </> file "paragraph2-image.png") Nothing Nothing
+    Paragraph3Png -> Location (Right $ rootDir </> dir "static" </> dir "images" </> file "paragraph3-image.png") Nothing Nothing
+    Paragraph4Png -> Location (Right $ rootDir </> dir "static" </> dir "images" </> file "paragraph4-image.png") Nothing Nothing
 
 
 data UserDetailsLinks
