8cd7435823757514bb2976ee904e6e3e65ddb6b5
Athan Clark <athan.clark@localcooking.com>
2018-04-11 12:23:33 -0600

    hopefully should be okay - odd linking error for tests

--- backend/test/Spec.hs
+++ backend/test/Spec.hs
@@ -3,7 +3,7 @@
   , ScopedTypeVariables
   #-}
 
-import Login.Error (AuthError, PreliminaryAuthToken)
+import LocalCooking.Login.Error (AuthError, PreliminaryAuthToken)
 import Links (SiteLinks)
 
 import Test.Tasty (defaultMain, testGroup)
@@ -11,13 +11,18 @@ import Test.Tasty.QuickCheck (testProperty)
 
 import Data.Aeson (FromJSON, ToJSON)
 import qualified Data.Aeson as Aeson
+import Path.Extended (ToLocation (..), FromLocation (..))
 
 
 main :: IO ()
-main = defaultMain $ testGroup "JSON encodings"
-  [ testProperty "AuthError" (\(x :: AuthError) -> jsonIso x)
-  , testProperty "PreliminaryAuthToken" (\(x :: PreliminaryAuthToken) -> jsonIso x)
-  , testProperty "SiteLinks" (\(x :: SiteLinks) -> jsonIso x)
+main = defaultMain $ testGroup "LocalCooking.com tests"
+  [ testGroup "JSON encodings"
+    [ testProperty "AuthError" (\(x :: AuthError) -> jsonIso x)
+    , testProperty "PreliminaryAuthToken" (\(x :: PreliminaryAuthToken) -> jsonIso x)
+    ]
+  , testGroup "Location encodings"
+    [ testProperty "SiteLinks" (\(x :: SiteLinks) -> locationIso x)
+    ]
   ]
 
 
@@ -25,3 +30,9 @@ jsonIso :: FromJSON a => ToJSON a => Eq a => a -> Bool
 jsonIso x = case Aeson.decode (Aeson.encode x) of
   Nothing -> False
   Just y -> x == y
+
+
+locationIso :: FromLocation a => ToLocation a => Eq a => a -> Bool
+locationIso x = case parseLocation (toLocation x) of
+  Left _ -> False
+  Right y -> x == y
