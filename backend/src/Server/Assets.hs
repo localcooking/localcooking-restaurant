b4febe823dda711b3b59a8235686355e238861e4
Athan Clark <athan.clark@localcooking.com>
2018-04-13 23:27:51 -0600

    latest logo

--- backend/src/Server/Assets.hs
+++ backend/src/Server/Assets.hs
@@ -18,7 +18,7 @@ frontendMin :: ByteString
 frontendMin = $(embedFile "../frontend/index.min.js")
 
 favicons :: [(FilePath, ByteString)]
-favicons = $(embedDir "../logo/localcooking-chef-favicon")
+favicons = $(embedDir "../logo/localcooking-farm-favicon")
 
 images :: [(FilePath, ByteString)]
 images = $(embedDir "images")
